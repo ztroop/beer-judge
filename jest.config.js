1b6d2fd24709f6b779ab23d35d1bcb505221720e
Zackary Troop <zack.tro@gmail.com>
2024-01-25 10:55:13 -0500

    Add transformer ignore rule in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   testMatch: ['<rootDir>/__tests__/**/*.test.tsx'],
+  transformIgnorePatterns: [],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json',
+      babelConfig: true,
+    },
+  },
 };
